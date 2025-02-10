e3c983030d8199af2413987b6ab0ab624fe28a9c
arun <arun@ceroen.com>
2025-02-10 15:04:40 +0530

    nG CONTENT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { RoomsListComponent } from './rooms/rooms-list/rooms-list.component';
 import { HeaderComponent } from './header/header.component';
+import { ContainerComponent } from './container/container.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 
 @NgModule({
@@ -14,6 +16,8 @@ import { HeaderComponent } from './header/header.component';
     RoomsComponent,
     RoomsListComponent,
     HeaderComponent,
+    ContainerComponent,
+    EmployeeComponent,
     
   ],
   imports: [
